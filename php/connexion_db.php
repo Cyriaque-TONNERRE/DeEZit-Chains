0cc5a0813e3bba607e43f4c08247c9ab7dc9a949
EdouardLambert <edouard.lambert@student.junia.com>
2022-05-10 15:52:15 +0200

    push1

---
+++ php/connexion_db.php
@@ -0,0 +1,10 @@
+<?php
+	$hostname = "localhost"; //nom du serveur (localhost)
+	$username = "root";//nom d'utilisateur pour acc�der au serveur (root)
+	$password = "root"; //mot de passe pour acc�der au serveur (root)
+	$dbname = "bijouterie"; //nom de la base de donn�es		
+
+	$connexion = mysqli_connect($hostname, $username, $password, $dbname);
+
+
+?>
