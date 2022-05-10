5d391825110e5926c1255b2e0e4b379e5928174d
Cdecaest <charles.decaestecker@student.junia.com>
2022-05-10 16:07:55 +0200

    add index
    page principale

---
+++ php/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+    <title> DeEZit Chain </title>
+    <meta name="description" content="Deezit Chain">
+    <meta name="keywords" content="Deezit Chain">
+    <meta name="author" content="Edouard LAMBERT, Lucas PORET, Charles DECAESTECKER, Cyriaque TONNERRE,Marc-Antoine LONGATTE">
+    <meta charset="UTF-8">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <header> <!-- On place dans cette balise le contenue de l'en-tête -->
+
+    </header>
+    <main>
+
+    </main>
+    <footer> 
+        
+    </footer>
+</body>
