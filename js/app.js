e8da552fa3576438b78b451c67171e9a4adcc44f
lucasPoret <lucas.poret@student.junia.com>
2022-05-11 13:54:42 +0200

    js drag and drop

---
+++ js/app.js
@@ -0,0 +1,56 @@
+
+const base = document.querySelector('.base');
+const box = document.querySelectorAll('.case');
+
+
+
+base.addEventListener('dragstart', dragStart);
+base.addEventListener('dragend', dragEnd);
+
+
+function dragStart() {
+    this.className += ' tenu';
+
+    setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+    this.className = 'base';
+}
+
+
+for (const vide of box) {
+
+    vide.addEventListener('dragover', dragOver);
+
+    vide.addEventListener('dragenter', dragEnter);
+
+    vide.addEventListener('dragleave', dragLeave);
+
+    vide.addEventListener('drop', dragDrop);
+
+
+}
+
+
+
+function dragOver(e) {
+    e.preventDefault()
+
+
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered';
+}
+
+function dragLeave() {
+    this.className = 'case';
+}
+
+
+function dragDrop() {
+    this.className = 'case';
+    this.append(base);
+}
