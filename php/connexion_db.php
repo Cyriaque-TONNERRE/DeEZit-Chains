bb2a60d31255f0658d0811962ff2573280992ffd
lucasPoret <lucas.poret@student.junia.com>
2022-05-11 09:26:48 +0200

    Update connexion_db.php

--- php/connexion_db.php
+++ php/connexion_db.php
@@ -2,7 +2,7 @@
 	$hostname = "localhost"; //nom du serveur (localhost)
 	$username = "root";//nom d'utilisateur pour acc�der au serveur (root)
 	$password = "root"; //mot de passe pour acc�der au serveur (root)
-	$dbname = "deezit chain"; //nom de la base de donn�es		
+	$dbname = "deezit_chain"; //nom de la base de donn�es		
 
 	$connexion = mysqli_connect($hostname, $username, $password, $dbname);
 
