14dbb8274b0ab562e98d5085cbc0705e2a14a04f
lucasPoret <lucas.poret@student.junia.com>
2022-05-11 09:35:59 +0200

    Update logout.php

--- php/logout.php
+++ php/logout.php
@@ -1,7 +1,7 @@
 <?php 
 		session_start(); //Démarrer la session
 	if(isset($_COOKIE["username"])){ // si un utilisateur est authentifié
-		setcookie("username","",-1);
+		setcookie("username","",0);
 		header("Location:login.php");
 	}
 ?>
