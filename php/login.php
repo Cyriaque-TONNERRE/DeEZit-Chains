d945c1c3e9c4d3c750b7ccffae8db37ad3c19fba
lucasPoret <lucas.poret@student.junia.com>
2022-05-11 09:07:54 +0200

    Update login.php

--- php/login.php
+++ php/login.php
@@ -24,7 +24,7 @@
 				
 				<form method="post" action="#">
 					<?php
-						if (isset($_COOKIE["ID"])) {
+						if (isset($_COOKIE["username"])) {
 							header("location:compte.php");
 						}
 						else {
