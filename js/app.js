cfe529efaf38220a3f0b10ab033088d92437bc9c
lucasPoret <lucas.poret@student.junia.com>
2022-05-11 16:03:35 +0200

    js comment

--- js/app.js
+++ js/app.js
@@ -1,33 +1,32 @@
-
-const base = document.querySelector('.base');
-const box = document.querySelectorAll('.case');
-
+const base = document.querySelector('.base'); //variable qui recup la base
+const box = document.querySelectorAll('.case'); //variable qui recup toutes les cases
 
 
-base.addEventListener('dragstart', dragStart);
-base.addEventListener('dragend', dragEnd);
+
+base.addEventListener('dragstart', dragStart); //event: lorsqu'on commence a drag appel la fonction dragStart
+base.addEventListener('dragend', dragEnd); //event: lorsqu'on lache l'objet appel la fonction dargEnd
 
 
-function dragStart() {
-    this.className += ' tenu';
+function dragStart() { // FONCTION dragStart
+    this.className += ' tenu'; //ajoute la class 'tenu' à l'objet actuel
 
-    setTimeout(() => (this.className = 'invisible'), 0);
+    setTimeout(() => (this.className = 'invisible'), 0); //permet de rendre l'objet invisible lorsqu'on drag sinon il reste afficher à son ancienne pos
 }
 
-function dragEnd() {
-    this.className = 'base';
+function dragEnd() { //FONCTION dragEnd
+    this.className = 'base'; //define la class de l'objet actuel a 'base'
 }
 
 
-for (const vide of box) {
+for (const vide of box) { //créer un event pour tout les élément de box
 
-    vide.addEventListener('dragover', dragOver);
+    vide.addEventListener('dragover', dragOver); //event: lorsqu'on est en train de drag
 
-    vide.addEventListener('dragenter', dragEnter);
+    vide.addEventListener('dragenter', dragEnter); //event: lorsqu'on est entré dans une zone ou on peut drop
 
-    vide.addEventListener('dragleave', dragLeave);
+    vide.addEventListener('dragleave', dragLeave); //event: lorsqu'on quitte une zone de drop
 
-    vide.addEventListener('drop', dragDrop);
+    vide.addEventListener('drop', dragDrop); //event: lorsqu'on drop l'item
 
 
 }
@@ -35,23 +34,22 @@ for (const vide of box) {
 
 
 function dragOver(e) {
-    e.preventDefault()
-
-
+    e.preventDefault(); //retire l'action par default de dragOver qu'on ne veut pas
 }
 
 function dragEnter(e) {
-    e.preventDefault();
-    this.className += 'hovered';
-    
+    e.preventDefault(); //retire l'action par default de dragEnter qu'on ne veut pas
+    this.className += ' hovered'; //ajoute la class hovered à l'objet actuel
+    this.append("add"); //ajout add dans la div actuel
+    console.log(textContent);
 }
 
 function dragLeave() {
-    this.className = 'case';
+    this.className = 'case'; //définie la class de l'objet actuel à ' case'
 }
 
 
 function dragDrop() {
-    this.className = 'case';
-    this.append(base);
+    this.className = 'case'; //définie la class de l'objet actuel à ' case'
+    this.append(base); //change la position de la base à l'élément actuel
 }
