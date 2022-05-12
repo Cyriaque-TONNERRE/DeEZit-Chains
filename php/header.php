8e0e91a55500e0ed5d00b9a6013823d328bf4cf4
Cyriaque-TONNERRE <cyriaque.tonnerre@student.junia.com>
2022-05-12 16:00:40 +0200

    Add header

---
+++ php/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+    <title> DeEZit Chain </title>
+    <meta name="description" content="Projet Informatique 2022">
+    <meta name="keywords" content="DeEZit, Chain, Game, Ez">
+    <meta name="author" content="Ez Team">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/header.css">
+
+</head>
+
+
+<body>
+
+<header>
+    <div class="logo">
+        <img src="../image/test.gif" alt="logo">
+    </div>
+    <div class="titre">
+        <h1> DeEZit Chain </h1>
+    </div>
+
+</header>
--- php/login.php
+++ php/login.php
@@ -1,38 +1,18 @@
-<!DOCTYPE html>
-
-<html lang="fr">
-<head>
-    <title> DeEZit Chain </title>
-    <meta name="description" content="Projet Informatique 2022">
-    <meta name="keywords" content="DeEZit, Chain, Game, Ez">
-    <meta name="author" content="Ez Team">
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/login.css">
-
-</head>
-
-
-<body>
-	
-    <header>
+<?php require './header.php'; ?>
+<main>
 		
-    </header>
-			
-	
-	<main>
-		
-		<div>
-			<nav class="formulaire">
+    <div>
+        <nav class="formulaire">
 				
-				<form method="post" action="#">
-					<fieldset>
-						<legend>Login :</legend>
-						<?php
-							if (isset($_COOKIE["ID"])) {
-								header("location:compte.php");
-							}
-							else {
-								echo '<label for="usernamehg">Username : </label >
+            <form method="post" action="#">
+                <fieldset>
+                    <legend>Login :</legend>
+                    <?php
+                    if (isset($_COOKIE["ID"])) {
+                        header("location:compte.php");
+                    }
+                    else {
+                        echo '<label for="usernamehg">Username : </label >
 								<input type="text" name="username" id="username" value="" required/>
 								<br><br>
 
@@ -45,51 +25,51 @@
 
 								<input type="submit" name="submit" id="submit" value="Send"/>
 								<input type="reset" value="Delete">';
-							}
-						?>
-					</fieldset>
-				<nav class="formulaire">
+                    }
+                    ?>
+                </fieldset>
+                <nav class="formulaire">
 				
-			<form method="post" action="#">
-		</div>	
+                    <form method="post" action="#">
+    </div>
 			
-	</main>
-		
+</main>
+
 <?php
-	include("connexion_db.php");
-	if (isset($_POST['submit'])){
-		$username = $_POST["username"];
-		$password = $_POST['password'];
-		$requete = "select * from user where username='$username'"; //selectionner le mail, password, et ID de la table membre
-		$resultat = mysqli_query($connexion,$requete);//executer la requete
-		if ($resultat == FALSE) {
-				echo "<p>Erreur d'exécution de la requete :".mysqli_error($connexion)."</p>";
-				die();
-		}
-		else {
-			$row = mysqli_fetch_assoc($resultat);
-			if (mysqli_num_rows($resultat) == 1 and password_verify($password,$row['password'])) {
-				setcookie("username", $row["username"], time() + (365*24*3600));
-				header("location:index.php");
-			}
-			else{
-				echo '<p class="error">Echec de connexion: identifiants incorrects</p>';
-			}
-		}
-	}
-	mysqli_close($connexion);
+include("connexion_db.php");
+if (isset($_POST['submit'])){
+    $username = $_POST["username"];
+    $password = $_POST['password'];
+    $requete = "select * from user where username='$username'"; //selectionner le mail, password, et ID de la table membre
+    $resultat = mysqli_query($connexion,$requete);//executer la requete
+    if ($resultat == FALSE) {
+        echo "<p>Erreur d'exécution de la requete :".mysqli_error($connexion)."</p>";
+        die();
+    }
+    else {
+        $row = mysqli_fetch_assoc($resultat);
+        if (mysqli_num_rows($resultat) == 1 and password_verify($password,$row['password'])) {
+            setcookie("username", $row["username"], time() + (365*24*3600));
+            header("location:index.php");
+        }
+        else{
+            echo '<p class="error">Echec de connexion: identifiants incorrects</p>';
+        }
+    }
+}
+mysqli_close($connexion);
 	
 	
 	
 
 ?>		
 		
-   <footer> <!-- On place dans cette balise le contenue du pied de page -->
+<footer> <!-- On place dans cette balise le contenue du pied de page -->
 	   	
-	</footer>    
+</footer>
   
 
 </body>
 
 
-</html>
+i</html>
