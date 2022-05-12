c976168719f271201ebbd32812618f3a9f6d196c
lucasPoret <lucas.poret@student.junia.com>
2022-05-12 11:51:30 +0200

    rectification des bordure avec js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
-const base = document.querySelector('.base'); //variable qui recup la base
+const red = document.querySelector('.r'); //variable qui recup la base
+const green = document.querySelector('.g'); //variable qui recup la base
 const box = document.querySelectorAll('.case'); //variable qui recup toutes les cases
 
 
 
-base.addEventListener('dragstart', dragStart); //event: lorsqu'on commence a drag appel la fonction dragStart
-base.addEventListener('dragend', dragEnd); //event: lorsqu'on lache l'objet appel la fonction dargEnd
-
+red.addEventListener('dragstart', dragStart); //event: lorsqu'on commence a drag appel la fonction dragStart
+red.addEventListener('dragend', dragEnd); //event: lorsqu'on lache l'objet appel la fonction dargEnd
+green.addEventListener('dragstart', dragStart); //event: lorsqu'on commence a drag appel la fonction dragStart
+green.addEventListener('dragend', dragEnd); //event: lorsqu'on lache l'objet appel la fonction dargEnd
 
 function dragStart() { // FONCTION dragStart
     this.className += ' tenu'; //ajoute la class 'tenu' à l'objet actuel
 
-    setTimeout(() => (this.className = 'invisible'), 0); //permet de rendre l'objet invisible lorsqu'on drag sinon il reste afficher à son ancienne pos
+    //setTimeout(() => (this.className = 'invisible'), 0); //permet de rendre l'objet invisible lorsqu'on drag sinon il reste afficher à son ancienne pos
 }
 
 function dragEnd() { //FONCTION dragEnd
@@ -39,23 +41,17 @@ function dragOver(e) {
 
 function dragEnter(e) {
     e.preventDefault(); //retire l'action par default de dragEnter qu'on ne veut pas
-    this.className += ' hovered'; //ajoute la class hovered à l'objet actuel
-    this.append("add");
-    console.log(document.getElementsByClassName('case').textContent);
-
-    /*if (textContent != "add") {
-        this.append("add"); //ajout add dans la div actuel
-        console.log("ajouter");
-    } else console.log("non");*/
+    //this.id += 'r2'; //ajoute la class 'r2' à l'objet actuel
+    console.log(this.id);
 }
 
 
 function dragLeave() {
-    this.className = 'case'; //définie la class de l'objet actuel à ' case'
+    this.className += 'case'; //définie la class de l'objet actuel à ' case'
 }
 
 
 function dragDrop() {
-    this.className = 'case'; //définie la class de l'objet actuel à ' case'
-    this.append(base); //change la position de la base à l'élément actuel
+    this.className += 'case'; //définie la class de l'objet actuel à ' case'
+    //this.append(red); //change la position de la base à l'élément actuel
 }
