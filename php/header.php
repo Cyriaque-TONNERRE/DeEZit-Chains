0eabd58f8221ac77aecdf76929d649d70f4b8f2f
Cyriaque-TONNERRE <cyriaque.tonnerre@student.junia.com>
2022-05-13 08:56:44 +0200

    Header

--- php/header.php
+++ php/header.php
@@ -15,11 +15,40 @@
 <body>
 
 <header>
-    <div class="logo">
-        <img src="../image/test.gif" alt="logo">
-    </div>
-    <div class="titre">
-        <h1> DeEZit Chain </h1>
-    </div>
+    <?php
+
+    if (!isset($_SESSION['connexion'])) {
+        echo '
+    <div class="connexion">
+        <div class="logo">
+            <img src="../image/test.gif" alt="logo">
+        </div>
+        <div class="titre">
+            <h1> DeEZit Chain </h1>
+        </div>
+        <div class="login">
+            <a href="login.php">
+                <img src="../image/login.svg" alt="login">
+            </a>
+        </div>
+    </div>';
+    } else {
+        echo '
+    <div class="connected">
+        <div class="logo">
+            <img src="../image/test.gif" alt="logo">
+        </div>
+        <div class="titre">
+            <h1> DeEZit Chain </h1>
+        </div>
+        <div class="register">
+            <a href="login.php">
+                <img src="../image/login.svg" alt="login">
+            </a>
+        </div>
+    </div>';
+    };
+
+?>
 
 </header>
