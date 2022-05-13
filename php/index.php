304b119d187600d216270ec1085a9ee6832b0e62
EdouardLambert <edouard.lambert@student.junia.com>
2022-05-13 09:42:12 +0200

    suppression contenue header dans index => require header.php

--- php/index.php
+++ php/index.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-
-<html lang="fr">
-<head>
-    <title> DeEZit Chain </title>
-    <meta name="description" content="Projet Informatique 2022">
-    <meta name="keywords" content="DeEZit, Chain, Game, Ez">
-    <meta name="author" content="Ez Team">
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/index.css">
-</head>
-
-<body>
-
-<header>
-    <?php require './header.php'; ?>
-</header>
-
+<?php require './header.php'; ?>
 
 <nav>
     <div class="menu">
