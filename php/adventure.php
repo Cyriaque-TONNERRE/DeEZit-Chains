1f9194c77f4c6131648944739049ec057cad483b
Cyriaque-TONNERRE <cyriaque.tonnerre@student.junia.com>
2022-05-18 13:08:49 +0200

    adventure

--- php/adventure.php
+++ php/adventure.php
@@ -1,36 +1,4 @@
-<?php
-session_start();
-?>
-<!DOCTYPE html>
-
-<html lang="fr">
-<head>
-    <title> DeEZit Chain </title>
-    <meta name="description" content="Projet Informatique 2022">
-    <meta name="keywords" content="DeEZit, Chain, Game, Ez">
-    <meta name="author" content="Ez Team">
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/index.css">
-    <link rel="stylesheet" href="../css/adventure.css">
-</head>
-<body>
-<header>
-
-</header>
-
-<nav>
-
-    <?php
-    
-    if (isset($_COOKIE["username"])) {
-        echo "<a href='logout.php'>Logout</a>";
-    }
-    else {
-        echo "<a href='login.php'>Login</a>";
-    }
-    ?>
-
-</nav>
+<?php require './header.php'; ?>
 
 <main>
     <?php
