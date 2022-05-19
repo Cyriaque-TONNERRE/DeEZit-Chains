a664d3317d7408b820400b291da12f7a0efb5b51
Cdecaest <charles.decaestecker@student.junia.com>
2022-05-19 14:57:40 +0200

    leaderboard update

--- php/leaderboard.php
+++ php/leaderboard.php
@@ -8,10 +8,7 @@
     <!-- Including our scripting file. -->
     <script type="text/javascript" src="../js/script.js"></script>
     <!-- Including CSS file. -->
-    <script type="text/css">
-        tr{
-        text-align: center;}
-    </script>
+    <link rel="stylesheet" src="../css/leaderboard.css"
 
 </head>
 <body>
@@ -23,56 +20,57 @@
 </body>
 <main>
 
-        <div>
-            <table><tr><th><h1>Leaderboard</h1></th></tr> </h1>
+    <div>
+        <table><tr><h1>Leaderboard</h1></tr> </h1>
 
-                <?php
-                require './connexion_db.php';
-                if ($connexion) {
-                    $requete = "SELECT username,adventure_lvl,time_trial FROM user ORDER BY adventure_lvl DESC limit 5";
-                    $resultat = mysqli_query($connexion, $requete); //Executer la requete
+            <?php
+            include("connexion_db.php");
+            $requete = "SELECT username,adventure_lvl,time_trial FROM user ORDER BY adventure_lvl DESC, time_trial ASC, username ASC limit 100";
+            $resultat = mysqli_query($connexion, $requete); //Executer la requete
+            if ($resultat == FALSE) {
+                echo "<p>Erreur d'exécution de la requete :".mysqli_error($connexion)."</p>";
+                die();
+            }
 
-                    //essaie d'ajout pour voir son classement dans le leaderboard (toutes les modifs en 2)
-                    //$requete2 = "SELECT username,adventure_lvl,time_trial from user where username=$_SESSION['username']";
-                    //$resultat2= mysqli_query($connexion, $requete2); //Executer la requete
-                }
-                if ($resultat == FALSE) {
-                    echo "<p>Erreur d'exécution de la requete :".mysqli_error($connexion)."</p>";
-                    die();
-                }
-                else {
-                    echo "<tr>
+                //essaie d'ajout pour voir son classement dans le leaderboard (toutes les modifs en 2)
+                //$requete2 = "SELECT username,adventure_lvl,time_trial from user where username=$_SESSION['username']";
+                //$resultat2= mysqli_query($connexion, $requete2); //Executer la requete
+
+
+            else {
+                echo "<tr>
 						<th>Rank</th>
 						<th>Username</th>
 						<th>Level</th>
 						<th>Time Trial</th>
 					</tr>";
-                    $nbreLignes = mysqli_num_rows($resultat); //Nombre de ligne du retour de la requete
-                    $i=0;
-                    if ($nbreLignes > 0) {
-                        if (isset($_POST['search'])) {
-                            //Search box value assigning to $Name variable.
-                            $Name = $_POST['search'];
-                            $Query = "SELECT username,history_lvl,adventure_lvl,time_trial FROM user WHERE username LIKE '%$Name%' LIMIT 5";
-                            //Query execution
-                            $ExecQuery = MySQLi_query($connexion, $Query);
-                            while($row_search = mysqli_fetch_array($ExecQuery)){
-                                echo "<tr>";
-                                foreach ($row_search as $colonne_search){
-                                    echo "<td>$colonne_search</td>";
-                                }
+                $nbreLignes = mysqli_num_rows($resultat); //Nombre de ligne du retour de la requete
+                $i=0;
+                if ($nbreLignes > 0) {
+                    while ($row = mysqli_fetch_assoc($resultat)) {
+                        echo "<tr>";
+                        $i += 1;
+                        if (isset($_SESSION['username'])){
+                            if ($row['username'] == $_SESSION['username']) {
+                                echo "<td class='active rank$i'>$i</td>";
+                                echo "<td class='active rank$i'>$row[username]</td>";
+                                echo "<td class='active rank$i'>$row[adventure_lvl]</td>";
+                                echo "<td class='active rank$i'>$row[time_trial]</td>";
                             }
-                        }
-                        while ($row = mysqli_fetch_assoc($resultat)) {
-
-                            echo "<tr>";
-                            $i+=1;
-                            foreach ($row as $colonne) {
-                                echo "<td>$i</td>";
-                                echo "<td>$colonne</td>";
-
-
+                            else{
+                                    echo "<td class='rank$i'>$i</td>";
+                                    echo "<td class='rank$i'>$row[username]</td>";
+                                    echo "<td class='rank$i'>$row[adventure_lvl]</td>";
+                                    echo "<td class='rank$i'>$row[time_trial]</td>";}
                             }
+
+                            else{
+                                    echo "<td class='rank$i'>$i</td>";
+                                    echo "<td class='rank$i'>$row[username]</td>";
+                                    echo "<td class='rank$i'>$row[adventure_lvl]</td>";
+                                    echo "<td class='rank$i'>$row[time_trial]</td>";}
+                            }
+
 
                             //$nbreLigne2 = mysqli_num_rows($resultat2); //Nombre de ligne du retour de la requete
                             //if ($nbreLigne2 > 0) {
@@ -84,13 +82,14 @@
 
                             //}
                         }
-                        echo "</table>";
-                        mysqli_close($connexion); //Fermer la connexion
                     }
-                }
+                    echo "</table>";
+                    mysqli_close($connexion); //Fermer la connexion
 
-                ?>
-        </div>
+
+
+            ?>
+    </div>
 
 </main>
 
