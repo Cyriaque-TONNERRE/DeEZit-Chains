248144355302cc09ed8a6f9ed7e6e81311536e83
Cdecaest <charles.decaestecker@student.junia.com>
2022-05-24 15:34:54 +0200

    update settings
    fonctionne normalement

--- php/setting.php
+++ php/setting.php
@@ -2,11 +2,12 @@
 
 <main>
 
+
     <div class="param_box">
         <h1>Settings:</h1>
         <div class="change_theme">
             Theme :
-            <span id="Dark">
+            <span id="Dark" onclick="Clique()">
                                 <?php if (isset($_COOKIE['theme'])) {
                                     if ($_COOKIE['theme'] == 'light') { ?>
                                         Dark
@@ -14,27 +15,33 @@
                                         Light
                                     <?php }
                                 } else { ?>
-                                    Dark
-                                <?php } ?>
+                                    Dark <?php } ?>
             </span>
         </div>
         <script>
-            document.getElementById("Dark").onclick = function () {
-                if (document.getElementById("Dark").innerHTML === 'Dark') {
+            function Clique() {
+                if (getCookie("theme") === null){
+                    document.getElementById("Dark").innerHTML = 'Light';
+                }
+                console.log("fonction");
+                var testData = document.getElementById("Dark");
+                console.log(testData);
+                if (document.getElementById("Dark").innerHTML === 'Light') {
+                    console.log("tamamanlagentille")
                     document.getElementById("header_theme").href = '../css/dark_header.css';
                     document.getElementById("body_theme").href = `../css/dark_setting.css`;
                     document.cookie = `theme=dark; expires=${new Date(new Date().getTime() + 31536000000).toUTCString()}; path=/`;
-                    document.getElementById("Dark").innerHTML = 'Light';
+                    document.getElementById("Dark").innerHTML = 'Dark';
                 } else {
+                    console.log("tamamanlamechante")
                     document.getElementById("body_theme").href = `../css/setting.css`;
                     document.getElementById("header_theme").href = '../css/header.css';
                     document.cookie = `theme=light; expires=${new Date(new Date().getTime() + 31536000000).toUTCString()}; path=/`;
-                    document.getElementById("Dark").innerHTML = 'Dark';
+                    document.getElementById("Dark").innerHTML = 'Light';
                 }
             }
-
-
         </script>
+
         <div id="Sound_title">
             <?php if (isset($_COOKIE['volume'])) {
                 $value = round($_COOKIE['volume'] * 100);
@@ -45,12 +52,11 @@
             ?>
         </div>
 
-        <?php if(isset($_COOKIE['volume'])){
+        <?php if (isset($_COOKIE['volume'])) {
             echo "<input type='range' min='0' value = '$value' max='100' id='Sound'>";
-        } else{
-             echo "<input type='range' min='0' value = '50' max='100' id='Sound'>";
-        }  ?>
-
+        } else {
+            echo "<input type='range' min='0' value = '50' max='100' id='Sound'>";
+        } ?>
 
 
         <script>
@@ -68,6 +74,3 @@
 </footer>
 
 
-</body>
-
-</html>
