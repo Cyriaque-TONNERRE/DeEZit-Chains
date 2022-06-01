b458efb765ac95042037ab3649e5dd532be82610
Cyriaque-TONNERRE <cyriaque.tonnerre@student.junia.com>
2022-06-01 12:00:23 +0200

    oups

--- js/purple.js
+++ js/purple.js
@@ -22,12 +22,6 @@ function dragStartp() { // FONCTION dragStart
     now = 'p';
     blocker = true;
 
-    ghostEle = document.createElement('div');
-    ghostEle.classList.add('dragging');
-    ghostEle.innerHTML ='.';
-    document.body.appendChild(ghostEle);
-    e.dataTransfer.setDragImage(ghostEle, 0, 0);
-
     //setTimeout(() => (this.className = 'invisible'), 0); //permet de rendre l'objet invisible lorsqu'on drag sinon il reste afficher à son ancienne pos
 }
 
